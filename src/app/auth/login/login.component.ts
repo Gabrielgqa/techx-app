f8d42c12f3f2c9cd0dda3836ece564091a2c9b3f
GabrielGQA <gabrielgqa@gmail.com>
2025-07-31 18:28:04 -0300

    feat: insere rota login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
+  standalone: true,
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  imports: [CommonModule, RouterModule, ReactiveFormsModule]
 })
 export class LoginComponent {
+  form: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      email: [''],
+      password: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    this.authService.login(this.form.value).subscribe({
+      next: () => this.router.navigate(['/tasks']),
+      error: (err) => {
+        alert(err.error.message || 'Falha no login');
+      }
+    });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
+  private api = 'http://localhost:3333/auth';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(data: { email: string; password: string }) {
+    return this.http.post<{ token: string }>(`${this.api}/login`, data).pipe(
+      tap(res => localStorage.setItem('token', res.token))
+    );
+  }
 }
